629eb3a9b4ebcd8b77a8605999fa81aaa08fee45
Quillbert <quillbert.quillbert@gmail.com>
2022-12-03 01:11:41 -0500

    added a better version of 2022 day 3 part 2

---
+++ 2022/day03b/src/better.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+	let input = fs::read_to_string("input.txt").unwrap();
+
+	let mut sum: u128 = 0;
+
+	let lines: Vec<_> = input.lines().collect();
+
+	for i in (0..lines.len()).into_iter().step_by(3) {
+		let i = i as usize;
+
+
+		let a: HashSet<char> = lines[i].chars().collect();
+		let b: HashSet<char> = lines[i + 1].chars().collect();
+		let c: HashSet<char> = lines[i + 2].chars().collect();
+				
+		let union1: HashSet<&char> = a.intersection(&b).collect();
+
+		let union2: HashSet<&char> = a.intersection(&c).collect();
+
+		let union3: HashSet<&&char> = union1.intersection(&union2).collect();
+
+		for ch in union3 {
+			let mut cha = **ch as u128;
+			println!("{}", ch);
+			if cha < 96 {
+				cha = cha - (65-27);
+				sum = sum + cha;
+			} else {
+				cha = cha - 96;
+				sum = sum + cha;
+			}
+
+		}
+
+	}
+	
+
+    println!("{}", sum);
+}
